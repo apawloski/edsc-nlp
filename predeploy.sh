c21f6f0ec515cc68568f4055e3904ae331bd8b62
Andrew Pawloski <Andrew@element84.com>
2016-09-15 15:55:17 -0400

    Simplified index downloading/decompression

--- predeploy.sh
+++ predeploy.sh
@@ -1,6 +1,4 @@
-echo "Downloading index.."
-aws s3 cp s3://gsfc-eosdis-ngap-us-east-1/prod/app/edsc-nlp/clavin-index.tar.gz .
-echo "Decompressing index.."
-tar xzf clavin-index.tar.gz
-rm clavin-index.tar.gz
+echo "Downloading and decompressing index.."
+aws s3 cp s3://gsfc-eosdis-ngap-us-east-1/prod/app/edsc-nlp/clavin-index.tar.gz - | tar xzf -
 echo "Index ready!"
+
